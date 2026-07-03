b47864a3995d32eb0a37204d70e29fd90df91071
Mohhitt45 <125465971+Mohhitt45@users.noreply.github.com>
2026-07-04 02:31:39 +0530

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -6,39 +6,42 @@ styles = getSampleStyleSheet()
 def create_report(result):
 
     pdf = SimpleDocTemplate("Resume_Report.pdf")
-
     story = []
 
-    parsed = result["resume_data"]
-
-    info = parsed["basic_info"]
-
     story.append(Paragraph("<b>AI Resume Intelligence Report</b>", styles["Title"]))
-
-    story.append(Paragraph(f"<br/>Name : {info['name']}", styles["Normal"]))
-    story.append(Paragraph(f"Email : {info['email']}", styles["Normal"]))
-    story.append(Paragraph(f"Phone : {info['phone']}", styles["Normal"]))
-
     story.append(Paragraph("<br/>", styles["Normal"]))
 
-    story.append(Paragraph(f"ATS Score : {parsed['ats_score']}%", styles["Heading2"]))
+    match_score = result.get("match_score", 0)
+    story.append(Paragraph(f"Job Match Score : {match_score}%", styles["Heading2"]))
 
-    story.append(Paragraph(f"Job Match Score : {result['match_score']}%", styles["Heading2"]))
+    if "resume_data" in result:
 
-    story.append(Paragraph("<br/><b>Matched Skills</b>", styles["Heading2"]))
+        parsed = result["resume_data"]
+        info = parsed.get("basic_info", {})
 
-    for skill in result["matched_skills"]:
-        story.append(Paragraph(skill, styles["Normal"]))
+        story.append(Paragraph(f"<br/>Name : {info.get('name','N/A')}", styles["Normal"]))
+        story.append(Paragraph(f"Email : {info.get('email','N/A')}", styles["Normal"]))
+        story.append(Paragraph(f"Phone : {info.get('phone','N/A')}", styles["Normal"]))
 
-    story.append(Paragraph("<br/><b>Missing Skills</b>", styles["Heading2"]))
+        ats = result.get("ats", {})
+        story.append(
+            Paragraph(
+                f"ATS Score : {ats.get('ats_score',0)}%",
+                styles["Heading2"]
+            )
+        )
 
-    for skill in result["missing_skills"]:
-        story.append(Paragraph(skill, styles["Normal"]))
+        story.append(Paragraph("<br/><b>Matched Skills</b>", styles["Heading2"]))
+        for skill in result.get("matched_skills", []):
+            story.append(Paragraph(skill, styles["Normal"]))
 
-    story.append(Paragraph("<br/><b>Extracted Skills</b>", styles["Heading2"]))
+        story.append(Paragraph("<br/><b>Missing Skills</b>", styles["Heading2"]))
+        for skill in result.get("missing_skills", []):
+            story.append(Paragraph(skill, styles["Normal"]))
 
-    for skill in parsed["skills"]:
-        story.append(Paragraph(skill, styles["Normal"]))
+        story.append(Paragraph("<br/><b>Extracted Skills</b>", styles["Heading2"]))
+        for skill in parsed.get("skills", []):
+            story.append(Paragraph(skill, styles["Normal"]))
 
     pdf.build(story)
 
