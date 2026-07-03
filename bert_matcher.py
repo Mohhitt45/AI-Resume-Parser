70651e1e4d07b85996557f5c18cbd02d897e94dc
Mohit Agrawal <agrawalmohit.ma@gmail.com>
2026-07-03 15:21:50 +0530

    Lazy load BERT model

--- bert_matcher.py
+++ bert_matcher.py
@@ -1,17 +1,19 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# lightweight but powerful model
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = None
 
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+    return model
 
 def calculate_bert_match(resume_text, job_text):
+    model = get_model()
 
-    # convert text → embeddings
     resume_vec = model.encode([resume_text])
     job_vec = model.encode([job_text])
 
-    # cosine similarity
     score = cosine_similarity(resume_vec, job_vec)[0][0]
-
     return round(score * 100, 2)
